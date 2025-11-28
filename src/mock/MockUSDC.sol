175de39916a77f68e6b15dd7716949ef5f9cf114
alessandropac96 <alessandro1.96@hotmail.it>
2025-11-28 11:19:37 +0100

    Update MockUSDC contract to mint 1M USDC with 18 decimals instead of 6

--- src/mock/MockUSDC.sol
+++ src/mock/MockUSDC.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract MockUSDC is ERC20Permit {
     constructor() ERC20Permit("Mock USDC") ERC20("Mock USDC", "USDC") {
-        _mint(msg.sender, 1_000_000 * 1e6); // 1M USDC, 6 decimals
+        _mint(msg.sender, 1_000_000 * 1e18); // 1M USDC, 18 decimals
     }
 
     function mint(address to, uint256 amount) external {
